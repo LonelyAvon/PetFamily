3e9481ac2a5feba16fe6bc34120ea59e20546f1b
LonelyAvon <92423766+LonelyAvon@users.noreply.github.com>
2024-04-19 20:29:25 +0700

    New Response
    a

--- Contracts/PetDto/Responses/GetAllPetsResponse.cs
+++ Contracts/PetDto/Responses/GetAllPetsResponse.cs
@@ -1,27 +1,6 @@
 ﻿using System;
 namespace Contracts.PetDto.Responses
 {
-	public record GetAllPetsResponse(
-        Guid Id,
-        string Nickname,
-        string Color,
-        string Description,
-        string Breed,
-        string Health,
-        string City,
-        string Street,
-        string Building,
-        string Index,
-        string Place,
-        float Weight,
-        DateTimeOffset BirthDate,
-        bool OnlyOneInFamily,
-        int Height,
-        string ContactPhoneNumber,
-        string VolunteerPhoneNumber,
-        string PeopleAttitude,
-        string AnimalAttitude,
-        bool OnTreatment,
-        bool Castration);
+	public record GetAllPetsResponse(List<PetDto> PetDtos);
 }
 
